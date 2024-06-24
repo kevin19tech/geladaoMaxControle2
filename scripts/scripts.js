9f4d251887bc71725830228ec521d1ec68974a33
Kevin <kevingomes793@gmail.com>
2024-06-23 23:48:31 -0300

    Lógica de subtrair e somar já estão funcionando

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,20 @@
 
-let qtd = document.getElementById()
+let qtd = document.getElementById("qtdGeladoes")
 let btnMenos = document.getElementById("buttonMenos");
 let btnMais = document.getElementById("buttonMais");
+
+let qtdAtual = Number(qtd.value);
+qtdAtual = 0
+
+function menos(){
+    if(qtdAtual > 0){
+        qtdAtual--
+        qtd.innerText = qtdAtual
+    }
+}
+
+function mais(){
+    qtdAtual++
+    qtd.innerText = qtdAtual
+}
+
