477cc44607b244d399c13cd2a76b7d452cddee08
Kevin Gomes <kevingomes793@gmail.com>
2024-06-28 16:52:32 -0300

    Exibindo 3 containers, com botões funcionando

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,15 @@
 let qtd = document.getElementById("qtdGeladoes")
 let qtd2 = document.getElementById("qtdGeladoes2")
+let qtd3 = document.getElementById("qtdGeladoes3")
 let btnMenos = document.getElementById("buttonMenos");
 let container2 = document.getElementById("container2");
+let container3 = document.getElementById("container3");
 let qtdAtual = Number(qtd.value);
 let qtd2Atual = Number(qtd2.value);
+let qtd3Atual = Number(qtd3.value);
 qtdAtual = 0
 qtd2Atual = 0
+qtd3Atual = 0
 
 function menos(index){
     //Primeiro botão menos
@@ -15,10 +19,10 @@ function menos(index){
     }else if(index === 2 && qtd2Atual > 0){ //Segundo botão menos
         qtd2Atual--
         qtd2.innerText = qtd2Atual
-    }/*else if(index === 3 && qtd3Atual > 0){ //Segundo botão menos
+    }else if(index === 3 && qtd3Atual > 0){ //Segundo botão menos
         qtd3Atual--
         qtd3.innerText = qtd3Atual
-    }*/
+    }
 }
 
 function mais(index){
@@ -29,10 +33,14 @@ function mais(index){
     } else if (index === 2){
         qtd2Atual++
         qtd2.innerText = qtd2Atual    
+    }else if (index === 3){
+        qtd3Atual++
+        qtd3.innerText = qtd3Atual    
     }
 }
 
 function addGeladao(){
     container2.style.display = 'flex';
+    container3.style.display = 'flex';
 }
 
