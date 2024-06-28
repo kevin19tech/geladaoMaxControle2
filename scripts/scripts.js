259e9a6a4a60b866148604f23ceb8535e32f6277
Kevin <kevingomes793@gmail.com>
2024-06-27 21:59:19 -0300

    Soma e subtracao da qtd dos dois botoes funcionando

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,21 +1,36 @@
 
 let qtd = document.getElementById("qtdGeladoes")
+let qtd2 = document.getElementById("qtdGeladoes2")
 let btnMenos = document.getElementById("buttonMenos");
-let btnMais = document.getElementById("buttonMais");
 let container2 = document.getElementById("container2");
 let qtdAtual = Number(qtd.value);
+let qtd2Atual = Number(qtd2.value);
 qtdAtual = 0
+qtd2Atual = 0
 
-function menos(){
-    if(qtdAtual > 0){
+function menos(index){
+    //Primeiro botão menos
+    if(index === 1 && qtdAtual > 0){
         qtdAtual--
         qtd.innerText = qtdAtual
-    }
+    }else if(index === 2 && qtd2Atual > 0){ //Segundo botão menos
+        qtd2Atual--
+        qtd2.innerText = qtd2Atual
+    }/*else if(index === 3 && qtd3Atual > 0){ //Segundo botão menos
+        qtd3Atual--
+        qtd3.innerText = qtd3Atual
+    }*/
 }
 
-function mais(){
-    qtdAtual++
-    qtd.innerText = qtdAtual
+function mais(index){
+    if (index === 1){
+        qtdAtual++
+        qtd.innerText = qtdAtual
+
+    } else if (index === 2){
+        qtd2Atual++
+        qtd2.innerText = qtd2Atual    
+    }
 }
 
 function addGeladao(){
